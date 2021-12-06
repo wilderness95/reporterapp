015781e9a2f43b26858c2d1c64cd129614f43afd
Widlerness Zoltan <horvzolt11@gmail.com>
2021-12-06 08:25:12 +0100

    Initial commit

---
+++ src/main/java/hu/wilderness/reporterapp/ReporterappApplication.java
@@ -0,0 +1,13 @@
+package hu.wilderness.reporterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporterappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReporterappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/wilderness/reporterapp/ReporterappApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.wilderness.reporterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporterappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
