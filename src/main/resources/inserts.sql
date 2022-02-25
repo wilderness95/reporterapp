7f3dde7f818a56dd685a9d8b9f5b7fa515639b1e
Zoltan Horvath <47075392+wilderness95@users.noreply.github.com>
2022-02-26 00:17:33 +0100

    Delete inserts.sql

--- src/main/resources/inserts.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email host', 'email.host', 'smtp.gmail.com');
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email port', 'email.port', '587');
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email user', 'email.user', 'elekteknikus@gmail.com');
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email password', 'email.password', 'AlmafaNarancsfa95');
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email smtp auth', 'email.smtp.auth', 'true');
-INSERT INTO parameter (active, description, value_key, value_data) VALUES (true, 'Email smtp start tls', 'email.smtp.starttls', 'true');
-INSERT INTO user(name, nick_name, email) VALUES ('wilder', 'widlerness', 'lordzolta@gmail.com');
-
-
